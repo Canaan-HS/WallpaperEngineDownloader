0d180c721b4b57318b6ff78a795ed9b8585d183f
TenshinoOtoKafu <s3459897@gmail.com>
2025-03-06 19:32:23 +0800

    完成整合功能

--- WallpaperDownloader.py
+++ WallpaperDownloader.py
@@ -4,6 +4,7 @@ import sys
 import time
 import locale
 import base64
+import textwrap
 import threading
 import subprocess
 import tkinter as tk
@@ -61,17 +62,19 @@ def language(lang):
 
 class DLL:
     def __init__(self):
-        self.folder = "Wallpaper_Output"
+        self.integrate_folder = "Integrate"
+        self.output_folder = "Wallpaper_Output"
         self.current_dir = Path(sys.executable).parent if getattr(sys, 'frozen', False) else Path(__file__).parent
 
-        self.save_path = Path(self.current_dir) / self.folder
+        self.save_path = Path(self.current_dir) / self.output_folder
         self.config_cfg = Path(self.current_dir) / "lastsavelocation.cfg"
+        self.icon_ico = Path(self.current_dir) / "Icon/DepotDownloader.ico"
         self.depot_exe = Path(self.current_dir) / "DepotdownloaderMod/DepotDownloadermod.exe"
 
         self.transl = language(locale.getlocale()[1])
 
         if not self.depot_exe.exists():
-            messagebox.showerror(self.transl('依賴錯誤'), f"{self.transl('找不到')} {self.depot_exe}")
+            messagebox.showerror(self.transl('依賴錯誤'), f"{self.transl('找不到')} {self.depot_exe}", parent=self)
             os._exit(0)
 
         if self.config_cfg.exists():
@@ -80,7 +83,7 @@ class DLL:
                 record_path = Path(record_str)
 
                 if record_path.is_absolute():
-                    self.save_path = record_path if record_path.name == self.folder else record_path / self.folder
+                    self.save_path = record_path if record_path.name == self.output_folder else record_path / self.output_folder
             except Exception as e:
                 print(f"{self.transl('讀取配置文件時出錯')}: {e}")
 
@@ -105,7 +108,7 @@ class DLL:
 
     def get_save_data(self):
         file_data = defaultdict(list)
-        [file_data[file.suffix].append(file) for file in self.save_path.rglob("*") if file.is_file()]
+        [file_data[file.suffix].append(file) for file in self.save_path.rglob("*") if file.is_file() and self.integrate_folder not in str(file)]
         return { # 數量多到少排序, 相同數量按字母排序, 組合 key 為副檔名, value 為檔案列表 回傳字典
             suffix: files for suffix, files in sorted(file_data.items(), key=lambda item: (-len(item[1]), item[0]))
         }
@@ -119,76 +122,76 @@ class GUI(DLL, tk.Tk):
         self.minsize(500, 550)
 
         try:
-            Icon = Path(self.current_dir) / "Icon/DepotDownloader.ico"
-            self.iconbitmap(Icon)
+            self.iconbitmap(self.icon_ico)
         except: pass
 
         self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
         self.primary_color = "#383d48"
+        self.consolo_color = "#272727"
         self.secondary_color = "#afd4ff"
         self.text_color = "#ffffff"
         self.configure(bg=self.primary_color)
 
-        self.top_frame = tk.Frame(self, bg=self.primary_color)
-        self.top_frame.grid(row=0, column=0, sticky="ew", padx=10, pady=5)
-        self.top_frame.columnconfigure(1, weight=0)
+        self.select_frame = tk.Frame(self, bg=self.primary_color)
+        self.select_frame.grid(row=0, column=0, sticky="ew", padx=10, pady=5)
+        self.select_frame.columnconfigure(1, weight=0)
         self.settings_element()
 
-        self.middle_frame = tk.Frame(self, bg=self.primary_color)
-        self.middle_frame.grid(row=1, column=0, sticky="nsew", padx=10, pady=5)
-        self.middle_frame.rowconfigure(1, weight=1)
-        self.middle_frame.columnconfigure(0, weight=1)
+        self.console_frame = tk.Frame(self, bg=self.primary_color)
+        self.console_frame.grid(row=1, column=0, sticky="nsew", padx=10, pady=5)
+        self.console_frame.rowconfigure(1, weight=1)
+        self.console_frame.columnconfigure(0, weight=1)
         self.display_element()
 
-        self.bottom_frame = tk.Frame(self, bg=self.primary_color)
-        self.bottom_frame.grid(row=2, column=0, sticky="ew", padx=10, pady=5)
-        self.bottom_frame.columnconfigure(0, weight=1)
+        self.operate_frame = tk.Frame(self, bg=self.primary_color)
+        self.operate_frame.grid(row=2, column=0, sticky="ew", padx=10, pady=5)
+        self.operate_frame.columnconfigure(0, weight=1)
         self.input_element()
 
     def settings_element(self):
-        username_label = tk.Label(self.top_frame, text=f"{self.transl('選擇帳號')}：", font=("Microsoft JhengHei", 14, "bold"), bg=self.primary_color, fg=self.text_color)
+        username_label = tk.Label(self.select_frame, text=f"{self.transl('選擇帳號')}：", font=("Microsoft JhengHei", 14, "bold"), bg=self.primary_color, fg=self.text_color)
         username_label.grid(row=0, column=0, sticky="w", pady=(10, 10))
 
         self.username = tk.StringVar(self)
         self.username.set(self.acclist[0])
-        self.sername_menu = ttk.Combobox(self.top_frame, textvariable=self.username, font=("Microsoft JhengHei", 10), cursor="hand2", justify="center", state="readonly", values=self.acclist)
+        self.sername_menu = ttk.Combobox(self.select_frame, textvariable=self.username, font=("Microsoft JhengHei", 10), cursor="hand2", justify="center", state="readonly", values=self.acclist)
         self.sername_menu.grid(row=0, column=1, sticky="w")
 
-        self.path_button = tk.Button(self.top_frame, text=self.transl('修改路徑'), font=("Microsoft JhengHei", 10, "bold"), cursor="hand2", relief="raised", bg=self.secondary_color, fg=self.text_color, command=self.save_settings)
+        self.path_button = tk.Button(self.select_frame, text=self.transl('修改路徑'), font=("Microsoft JhengHei", 10, "bold"), cursor="hand2", relief="raised", bg=self.secondary_color, fg=self.text_color, command=self.save_settings)
         self.path_button.grid(row=1, column=0, sticky="w")
 
-        self.save_path_label = tk.Label(self.top_frame, text=self.save_path, font=("Microsoft JhengHei", 14, "bold"), bg=self.primary_color, fg=self.text_color)
+        self.save_path_label = tk.Label(self.select_frame, text=self.save_path, font=("Microsoft JhengHei", 14, "bold"), bg=self.primary_color, fg=self.text_color)
         self.save_path_label.grid(row=1, column=1, sticky="w")
 
-        self.merge_button = tk.Button(self.top_frame, text=self.transl('檔案整合'), font=("Microsoft JhengHei", 10, "bold"), cursor="hand2", relief="raised", bg=self.secondary_color, fg=self.text_color, command=self.file_merge)
+        self.merge_button = tk.Button(self.select_frame, text=self.transl('檔案整合'), font=("Microsoft JhengHei", 10, "bold"), cursor="hand2", relief="raised", bg=self.secondary_color, fg=self.text_color, command=self.file_merge)
         self.merge_button.grid(row=2, column=0, sticky="w", pady=(15, 0))
 
     def display_element(self):
-        console_label = tk.Label(self.middle_frame, text=f"{self.transl('控制台輸出')}：", font=("Microsoft JhengHei", 10, "bold"), bg=self.primary_color, fg=self.text_color)
+        console_label = tk.Label(self.console_frame, text=f"{self.transl('控制台輸出')}：", font=("Microsoft JhengHei", 10, "bold"), bg=self.primary_color, fg=self.text_color)
         console_label.grid(row=0, column=0, sticky="nsew")
 
-        self.console = scrolledtext.ScrolledText(self.middle_frame, font=("Consolas", 12), height=14, borderwidth=4, cursor="arrow", relief="sunken", state="disabled", bg="#272727", fg=self.text_color)
+        self.console = scrolledtext.ScrolledText(self.console_frame, font=("Consolas", 12), height=14, borderwidth=4, cursor="arrow", relief="sunken", state="disabled", bg=self.consolo_color, fg=self.text_color)
         self.console.tag_configure("important", foreground="#00DB00", font=("Consolas", 12, "bold"))
         self.console.grid(row=1, column=0, sticky="nsew")
 
     def input_element(self):
-        input_label = tk.Label(self.bottom_frame, text=f"{self.transl('輸入創意工坊專案（每行一個，支援連結和檔案ID）')}：", font=("Microsoft JhengHei", 10, "bold"), bg=self.primary_color, fg=self.text_color)
+        input_label = tk.Label(self.operate_frame, text=f"{self.transl('輸入創意工坊專案（每行一個，支援連結和檔案ID）')}：", font=("Microsoft JhengHei", 10, "bold"), bg=self.primary_color, fg=self.text_color)
         input_label.grid(row=0, column=0, sticky="nsew")
 
-        self.input_text = scrolledtext.ScrolledText(self.bottom_frame, font=("Microsoft JhengHei", 10, "bold"), height=7, borderwidth=2, relief="sunken", wrap="none")
+        self.input_text = scrolledtext.ScrolledText(self.operate_frame, font=("Microsoft JhengHei", 10, "bold"), height=7, borderwidth=2, relief="sunken", wrap="none")
         self.input_text.grid(row=1, column=0, sticky="nsew")
         threading.Thread(target=self.listen_clipboard).start()
 
-        self.run_button = tk.Button(self.bottom_frame, text=self.transl('下載'), font=("Microsoft JhengHei", 14, "bold"), borderwidth=2, cursor="hand2", relief="raised", bg=self.secondary_color, fg=self.text_color, command=self.download_trigger)
+        self.run_button = tk.Button(self.operate_frame, text=self.transl('下載'), font=("Microsoft JhengHei", 14, "bold"), borderwidth=2, cursor="hand2", relief="raised", bg=self.secondary_color, fg=self.text_color, command=self.download_trigger)
         self.run_button.grid(row=2, column=0, sticky="ew", pady=(12, 5))
 
     def save_settings(self):
         path = filedialog.askdirectory(title=self.transl('選擇資料夾'))
 
         if path:
-            self.save_path = Path(path) / self.folder
+            self.save_path = Path(path) / self.output_folder
             self.save_path_label.config(text=self.save_path)
             self.config_cfg.write_text(str(self.save_path), encoding="utf-8")
 
@@ -196,9 +199,78 @@ class GUI(DLL, tk.Tk):
         data_table = self.get_save_data()
 
         if data_table:
-            messagebox.showinfo("wait", "Not yet developed")
+            merge_window = tk.Toplevel(self)
+            merge_window.title(self.transl('檔案整合'))
+            merge_window.configure(bg=self.primary_color)
+            
+            try:
+                merge_window.iconbitmap(self.icon_ico)
+            except: pass
+
+            width = 500
+            height = 550
+
+            merge_window.geometry(f"{width}x{height}+{int((self.winfo_screenwidth() - width) / 2)}+{int((self.winfo_screenheight() - height) / 2)}")
+            merge_window.minsize(400, 450)
+
+            tip_frame = tk.Frame(merge_window, bg=self.primary_color)
+            tip_frame.pack(fill="x", padx=10, pady=10)
+            tip = tk.Label(tip_frame, text=self.transl('選擇整合的類型'), font=("Microsoft JhengHei", 18, "bold"), bg=self.primary_color, fg=self.text_color)
+            tip.pack()
+
+            display_frame = tk.Frame(merge_window, bg=self.primary_color)
+            display_frame.pack(fill="both", expand=True, padx=10, pady=10)
+
+            output_frame = tk.Frame(merge_window, bg=self.primary_color)
+            output_frame.pack(fill="x")
+
+            scroll_y = tk.Scrollbar(display_frame, orient="vertical")
+            scroll_y.pack(side="right", fill="y")
+
+            style = ttk.Style()
+            style.configure("Custom.Treeview", font=("Microsoft JhengHei", 14, "bold"), foreground=self.text_color, background=self.consolo_color, rowheight=30)
+            style.configure("Custom.Treeview.Heading", font=("Microsoft JhengHei", 16, "bold"), foreground="#0066CC")
+
+            treeview = ttk.Treeview(display_frame, columns=("Type", "Count"), show="headings", yscrollcommand=scroll_y.set, cursor="hand2", style="Custom.Treeview")
+            treeview.heading("Type", text=self.transl('檔案類型'))
+            treeview.heading("Count", text=self.transl('檔案數量'))
+            treeview.column("Type", anchor="center")
+            treeview.column("Count", anchor="center")
+
+            for key, value in data_table.items():
+                treeview.insert("", "end", values=(key.replace(".", ""), len(value)))
+
+            scroll_y.config(command=treeview.yview)
+            treeview.pack(fill="both", expand=True)
+
+            def move_save_file():
+                if len(treeview.selection()) == 0:
+                    messagebox.showwarning(title=self.transl('操作提示'), message=self.transl('請選擇要整合的類型'), parent=merge_window)
+                    return
+
+                selected = []
+                selected_items = treeview.selection()
+
+                for item in selected_items:
+                    values = treeview.item(item, "values")  # 取得對應的數據
+                    selected.append(values[0])
+
+                confirm = messagebox.askquestion(self.transl('操作確認'), f"{self.transl('整合以下類型的檔案')}?\n\n{selected}", parent=merge_window)
+                if confirm == "yes":
+                    for item in selected_items: # 移除選中的項目
+                        treeview.delete(item)
+
+                    merge_path = self.save_path / self.integrate_folder
+                    merge_path.mkdir(parents=True, exist_ok=True)
+                    move_file = [data_table[f".{select}"][0] for select in selected]
+
+                    for files in move_file:
+                        files.rename(merge_path / f"[{files.parent.name}] {files.name}")
+
+            print_button = tk.Button(output_frame, text=self.transl('整合選中的類型'), font=("Microsoft JhengHei", 12, "bold"), borderwidth=2, cursor="hand2", relief="raised", bg=self.secondary_color, fg=self.text_color, command=move_save_file)
+            print_button.pack(pady=(5, 15))
         else:
-            messagebox.showwarning(title=self.transl('獲取失敗'), message=self.transl('沒有可整合的檔案'))
+            messagebox.showwarning(title=self.transl('獲取失敗'), message=self.transl('沒有可整合的檔案'), parent=self)
 
     def status_switch(self, state):
         if state == "disabled":
