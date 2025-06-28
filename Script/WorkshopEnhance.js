f761e938ab52cc7acbb695eccaa0edc4ebc29198
Canaan-HS <s3459897@gmail.com>
2025-06-28 09:03:22 +0800

    1. 增加快捷換頁 2. 修正 workshop 載入處理

--- Script/WorkshopEnhance.js
+++ Script/WorkshopEnhance.js
@@ -12,11 +12,13 @@
 // @icon https://cdn-icons-png.flaticon.com/512/220/220608.png
 
 // @noframes
-// @run-at document-body
 // @grant none
+
+// @run-at document-start
 // ==/UserScript==
 
 (async () => {
+
     const url = location.href;
     const app = /^https:\/\/steamcommunity\.com\/app\/\d+/;
     const workshop = /^https:\/\/steamcommunity\.com\/workshop\/browse\/\?appid=\d+/;
@@ -31,14 +33,46 @@
         })
     } else if (workshop.test(url)) {
         WaitElem(".workshopBrowseItems", items => {
-            items.querySelectorAll("div.workshopItem").forEach(div => {
-                const title = div.querySelector(".workshopItemTitle")?.textContent;
-                title && div.querySelectorAll("a").forEach(a => ReUri(a, title));
+            WaitLoad(items, 300, () => {
+                items.querySelectorAll(".workshopItem").forEach(div => {
+                    const title = div.querySelector(".workshopItemTitle")?.textContent;
+                    title && div.querySelectorAll("a").forEach(a => ReUri(a, title));
+                })
             })
         })
+
+        WaitElem(".pagebtn", buttons => {
+            if (buttons.length === 2) {
+                let Jump = false;
+
+                const TurnPage = (Url) => {
+                    Jump = true;
+                    location.assign(Url);
+                };
+
+                window.addEventListener("keydown", event => {
+                    const key = event.key;
+                    if (key === "ArrowLeft" && buttons[0].hasAttribute("href") && !Jump) {
+                        TurnPage(buttons[0].href);
+                    } else if (key === "ArrowRight" && buttons[1].hasAttribute("href") && !Jump) {
+                        TurnPage(buttons[1].href);
+                    }
+                }, { capture: true });
+            }
+        }, true)
     } else if (sharedfiles.test(url)) {
         WaitElem(".workshopItemTitle", title => ReUri(url, title?.textContent ?? ""));
     }
+
+    function Debounce(func, delay) {
+        let timer = null;
+        return (...args) => {
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                func(...args);
+            }, delay);
+        }
+    };
 
     function ReUri(uri, title) {
         const isElement = uri instanceof Element;
@@ -51,14 +85,20 @@
         isElement ? uri.href = newUri : history.replaceState(null, '', newUri);
     };
 
-    function WaitElem(selector, found) {
+    function WaitLoad(container, debounce, run) {
+        const observer = new MutationObserver(Debounce(() => { run() }, debounce));
+        observer.observe(container, { subtree: true, childList: true, attributes: true, characterData: true });
+    };
+
+    function WaitElem(selector, found, all = false) {
         const observer = new MutationObserver(() => {
-            const result = document.querySelector(selector);
-            if (result) {
+            const result = all ? document.querySelectorAll(selector) : document.querySelector(selector);
+            if (all ? result.length > 0 : result) {
                 observer.disconnect();
                 found(result);
             }
         });
-        observer.observe(document.body, {subtree: true, childList: true});
+        observer.observe(document, { subtree: true, childList: true });
     };
+
 })();
