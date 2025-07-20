16bee1d2cb79610f6f6808a062a99262345835a7
Canaan-HS <s3459897@gmail.com>
2025-07-21 04:43:17 +0800

    lib 導入

---
+++ modules/lite/bootstrap/libs.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import shutil
+import atexit
+import platform
+import traceback
+import threading
+import subprocess
+import tkinter as tk
+
+import re
+import json
+import time
+import base64
+import locale
+import ctypes
+import logging
+import functools
+
+from pathlib import Path
+from operator import itemgetter
+from urllib.parse import unquote
+from types import SimpleNamespace
+from collections import deque, defaultdict
+from tkinter import ttk, scrolledtext, filedialog, messagebox
+
+import psutil
+import pyperclip
