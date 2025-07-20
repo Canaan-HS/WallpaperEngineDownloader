1279f83b2063cc595b5ba259736aaf7e2ca0299f
Canaan-HS <s3459897@gmail.com>
2025-07-21 02:14:02 +0800

    依賴庫

---
+++ LiteModules/Libs.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import shutil
+import atexit
+import platform
+import traceback
+import threading
+import subprocess
+import tkinter as tk
+
+import re
+import json
+import time
+import base64
+import locale
+import ctypes
+import logging
+import functools
+
+from pathlib import Path
+from operator import itemgetter
+from urllib.parse import unquote
+from types import SimpleNamespace
+from collections import deque, defaultdict
+from tkinter import ttk, scrolledtext, filedialog, messagebox
+
+import psutil
+import pyperclip
+
+SysPlat = platform.system()
+
+
+def Elapsed_Time(stime=None, lable=""):
+    """
+    stime = Elapsed_Time()
+    run code ...
+    Elapsed_Time(stime, "code name")
+    """
+    if stime is None:
+        return time.perf_counter()
+    print(f"{lable} Elapsed: {time.perf_counter() - stime} s".strip())
