ed786ac911ce62506458839228598c421e1b2ec5
Canaan-HS <s3459897@gmail.com>
2025-08-11 04:22:41 +0800

    shared

---
+++ Modules/lite/core/shared.py
@@ -0,0 +1,41 @@
+from ..bootstrap import json, logging
+
+transl = None
+
+ck = None
+cfg_key = None
+cfg_data = None
+
+output_folder = None
+integrate_folder = None
+
+icon_ico = None
+save_path = None
+
+config_json = None
+
+repkg = None
+repkg_exe = None
+depot_exe = None
+
+account = None
+appid_dict = None
+
+
+def save_config(data):
+    old_data = {}
+    cache_data = ""
+
+    if config_json.exists():
+        try:
+            cache_data = (old_data := json.loads(config_json.read_text(encoding="utf-8"))).copy()
+        except Exception as e:
+            logging.info(e)
+
+    old_data.update(data)
+    final_config = {val: old_data.get(val, "") for val in cfg_key.values() if val in old_data}
+
+    if cache_data != final_config:
+        config_json.write_text(
+            json.dumps(old_data, indent=4, separators=(",", ":")), encoding="utf-8"
+        )
