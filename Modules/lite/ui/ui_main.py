d53e6ae18ff7855f8ec705b5856d59ff538f6aee
Canaan-HS <s3459897@gmail.com>
2025-08-11 14:39:30 +0800

    ui_main

--- Modules/lite/ui/ui_main.py
+++ Modules/lite/ui/ui_main.py
@@ -14,7 +14,7 @@ from ..core import shared
 from ..utils import account_list, get_ext_groups
 
 
-class UI:
+class UI_Main:
     def __init__(self):  # 方便還原
         self.win_title = f"Wallpaper Engine {shared.transl('創意工坊下載器')}"
 
@@ -70,24 +70,6 @@ class UI:
 
         shared.msg.connect(lambda: self.username.get().strip(), "username")
         shared.msg.connect(lambda: self.serverid.get().strip(), "serverid")
-
-        # 目前只用於顯示 請求速度
-        shared.msg.connect(
-            lambda speed="": self.title(
-                f"{self.win_title} （{speed}）" if speed.strip() else self.win_title
-            ),
-            "title_change",
-        )
-
-        shared.msg.connect(
-            lambda title, message: messagebox.showerror(title, message, parent=self),
-            "showerror",
-        )
-
-        shared.msg.connect(self.ui_close)
-        shared.msg.connect(self.input_operat)
-        shared.msg.connect(self.console_insert)
-        shared.msg.connect(self.button_state_change)
 
     """ ====== 主要元件 ====== """
 
@@ -231,81 +213,6 @@ class UI:
             command=lambda: threading.Thread(target=self.download_trigger, daemon=True).start(),
         )
         self.run_button.grid(row=2, column=0, sticky="ew", pady=(12, 5))
-
-    """ ====== UI 交互 ====== """
-
-    def ui_close(self, account, application, tasks):
-        shared.save_config(
-            {
-                "Account": account,
-                "Application": application,
-                "window_x": self.winfo_x(),
-                "window_y": self.winfo_y(),
-                "window_width": self.winfo_width(),
-                "window_height": self.winfo_height(),
-                "Tasks": tasks,
-            }
-        )
-
-        self.destroy()
-
-    def console_insert(self, message: str, *args):
-        self.console.config(state="normal")
-        self.console.insert("end", message, *args)
-        self.console.yview("end")
-        self.console.config(state="disabled")
-
-    def button_state_change(self, state, cursor):
-        self.merge_button.config(state=state, cursor=cursor)
-        self.run_button.config(state=state, cursor=cursor)
-
-    def input_operat(self, operat: str, *args):
-        if operat == "insert":
-            self.input_text.insert("end", *args)
-            self.input_text.yview("end")
-            self.input_text.xview_moveto(1.0)
-            self.input_text.update_idletasks()
-        elif operat == "delete":
-            self.input_text.delete(*args)
-        elif operat == "get":
-            return self.input_text.get(*args).splitlines()
-
-    def search_operat(self):
-        # 文本緩存, 用於當取消焦點狀態 且 serverid 內容為空時, 重新填充
-        self.text_cache = ""
-
-        def on_input(event):
-            widget = event.widget
-            suffix = widget.get().lower()  # 忽略大小寫
-            widget.configure(values=self.search_list(suffix))
-
-        def on_click(event):
-            if self.searcher is None:
-                self.after(100, self.build_searcher)
-
-            x = event.x
-            widget = event.widget
-            text = self.serverid.get()
-
-            if x < widget.winfo_width() - 20 and "->" in text:
-                text = self.clean_text(text)
-                self.serverid.set(text)
-                widget.unbind("<Button-1>")
-
-            self.text_cache = text
-
-        def on_select(event):
-            self.text_cache = self.serverid.get()
-            event.widget.configure(values=self.app_list)
-
-        def of_select(_):
-            if self.serverid.get().strip() == "":
-                self.serverid.set(self.text_cache)
-
-        self.serverid_menu.bind("<KeyRelease>", on_input)
-        self.serverid_menu.bind("<Button-1>", on_click)
-        self.serverid_menu.bind("<FocusOut>", of_select)
-        self.serverid_menu.bind("<<ComboboxSelected>>", on_select)
 
     """ ====== 其餘功能 ====== """
 
