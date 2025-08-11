a2bf50da7a134965033168af40fb0801226af150
Canaan-HS <s3459897@gmail.com>
2025-08-11 14:39:23 +0800

    ui_loader

---
+++ Modules/lite/ui/ui_loader.py
@@ -0,0 +1,16 @@
+from ..bootstrap import tk
+from ..core import Backend
+
+from .ui_main import UI_Main
+from .ui_operat import UI_Operat
+
+
+class UI_Loader(tk.Tk, Backend, UI_Main, UI_Operat):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        Backend.__init__(self)
+        UI_Main.__init__(self)
+        UI_Operat.__init__(self)
+
+        self.protocol("WM_DELETE_WINDOW", self.closure)  # 關閉 GUI 與 自動保存
+        self.mainloop()
