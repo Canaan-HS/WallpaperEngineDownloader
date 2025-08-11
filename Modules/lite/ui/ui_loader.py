ce520fa37f4b0ae430c3884f6138929100fe7967
Canaan-HS <s3459897@gmail.com>
2025-08-11 15:59:21 +0800

    變更導入名稱

--- Modules/lite/ui/ui_loader.py
+++ Modules/lite/ui/ui_loader.py
@@ -1,14 +1,14 @@
 from ..bootstrap import tk
-from ..core import Backend
+from ..core import Backend_Loader
 
 from .ui_main import UI_Main
 from .ui_operat import UI_Operat
 
 
-class UI_Loader(tk.Tk, Backend, UI_Main, UI_Operat):
+class UI_Loader(tk.Tk, Backend_Loader, UI_Main, UI_Operat):
     def __init__(self):
         tk.Tk.__init__(self)
-        Backend.__init__(self)
+        Backend_Loader.__init__(self)
         UI_Main.__init__(self)
         UI_Operat.__init__(self)
 
