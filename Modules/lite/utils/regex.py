e08040063a3872cf86d76af9881bd8cacf1358e4
Canaan-HS <s3459897@gmail.com>
2025-08-13 18:57:43 +0800

    增加 ID 自動捕獲

--- Modules/lite/utils/regex.py
+++ Modules/lite/utils/regex.py
@@ -2,4 +2,4 @@ from ..bootstrap import re
 
 illegal_regex = re.compile(r'[<>:"/\\|?*\x00-\x1F]')
 parse_regex = re.compile(r"(\d{8,10})(?:&searchtext=(.*))?")
-link_regex = re.compile(r"^https://steamcommunity\.com/sharedfiles/filedetails/\?id=\d+.*$")
+link_regex = re.compile(r"^(?:https://steamcommunity\.com/sharedfiles/filedetails/\?id=\d{8,10}.*|\d{8,10})$")
