ecc748b366686731ba542cfe9ff3b1d3c10719ca
Canaan-HS <s3459897@gmail.com>
2026-06-24 10:54:12 +0800

    2026/06/24

--- Script/WorkshopEnhance.js
+++ Script/WorkshopEnhance.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name WorkshopEnhance
-// @version 2025/07/06
+// @version 2026/06/24
 // @author Canaan HS
 // @description 一個簡單的工作坊網址替換腳本，為網址添加 searchtext=<標題>
 // @description:zh-TW 一個簡單的工作坊網址替換腳本，為網址添加 searchtext=<標題>
@@ -12,60 +12,49 @@
 // @icon https://cdn-icons-png.flaticon.com/512/220/220608.png
 
 // @noframes
-// @grant none
+// @grant window.onurlchange
 
 // @run-at document-start
 // ==/UserScript==
 
-(async () => {
-
-    const url = location.href;
+(() => {
     const app = /^https:\/\/steamcommunity\.com\/app\/\d+/;
     const workshop = /^https:\/\/steamcommunity\.com\/workshop\/browse\/\?appid=\d+/;
     const sharedfiles = /^https:\/\/steamcommunity\.com\/sharedfiles\/filedetails\/\?id=\d+/;
     const myworkshopfiles = /^https:\/\/steamcommunity\.com\/profiles\/\d+\/myworkshopfiles\/?.*$/;
 
-    if (app.test(url)) {
-        WaitElem(".workshop_home_content", content => {
-            content.querySelectorAll("a.workshop_item_link").forEach(a => {
-                const title = a.querySelector(".workshop_item_title")?.textContent;
-                title && ReUri(a, title);
-            })
-        })
-    } else if (workshop.test(url) || myworkshopfiles.test(url)) {
-        WaitElem(".workshopBrowseItems", items => {
-            WaitLoad(items, 300, () => {
-                items.querySelectorAll(".workshopItem").forEach(div => {
-                    const title = div.querySelector(".workshopItemTitle")?.textContent;
-                    title && div.querySelectorAll("a:not(.workshop_author_link)").forEach(a => ReUri(a, title));
+    let jumpMark = false;
+
+    function main(url) {
+        if (app.test(url)) {
+            waitElem("div[style*='--gap: var(--spacing-9);'] div.Panel", findUri)
+        }
+        else if (workshop.test(url) || myworkshopfiles.test(url)) {
+            waitElem("div[style*='--gap: var(--spacing-5);']", container => {
+                waitLoad(container, 300, () => {
+                    findUri();
+                    waitElem("button[data-accent-color='accent']", buttons => {
+                        if (buttons.length === 2) {
+                            // 當跳轉標記為 true 時，代表先前觸發過，因此移除先前鍵盤監聽
+                            if (jumpMark) window.removeEventListener("keydown", triggerTurnPage);
+
+                            jumpMark = false;
+                            window.addEventListener(
+                                "keydown",
+                                event => triggerTurnPage(event, buttons),
+                                { capture: true }
+                            );
+                        }
+                    }, true)
                 })
             })
-        })
+        }
+        else if (sharedfiles.test(url)) {
+            waitElem(".workshopItemTitle", title => reUri(url, title?.textContent ?? ""));
+        }
+    };
 
-        WaitElem(".pagebtn", buttons => {
-            if (buttons.length === 2) {
-                let Jump = false;
-
-                const TurnPage = (Url) => {
-                    Jump = true;
-                    location.assign(Url);
-                };
-
-                window.addEventListener("keydown", event => {
-                    const key = event.key;
-                    if (key === "ArrowLeft" && buttons[0].hasAttribute("href") && !Jump) {
-                        TurnPage(buttons[0].href);
-                    } else if (key === "ArrowRight" && buttons[1].hasAttribute("href") && !Jump) {
-                        TurnPage(buttons[1].href);
-                    }
-                }, { capture: true });
-            }
-        }, true)
-    } else if (sharedfiles.test(url)) {
-        WaitElem(".workshopItemTitle", title => ReUri(url, title?.textContent ?? ""));
-    }
-
-    function Debounce(func, delay) {
+    function _debounce(func, delay) {
         let timer = null;
         return (...args) => {
             clearTimeout(timer);
@@ -75,23 +64,54 @@
         }
     };
 
-    function ReUri(uri, title) {
+    function findUri() {
+        const links = document.querySelectorAll("a[href^='https://steamcommunity.com/sharedfiles/filedetails/?id=']");
+        if (links.length % 2 === 0) {
+            for (let i = 0; i < links.length; i += 2) {
+                const [rawLink, titleLink] = [links[i], links[i + 1]];
+                const titleText = titleLink.textContent.trim();
+
+                if (!titleText) continue;
+
+                reUri(rawLink, titleText);
+                reUri(titleLink, titleText);
+            }
+        }
+    };
+
+    function reUri(uri, title) {
         const isElement = uri instanceof Element;
         const Uri = isElement ? uri.href : uri;
 
-        const newUri = Uri.includes("&searchtext=")
-            ? Uri.replace(/(&searchtext=)(.*)?/, `$1${title}`)
-            : `${Uri}&searchtext=${title}`;
+        const url = new URL(Uri);
+        url.searchParams.set("searchtext", title);
 
-        isElement ? uri.href = newUri : history.replaceState(null, '', newUri);
+        const newUri = url.href.replace(/\+/g, " ");
+
+        isElement
+            ? uri.tagName === "A" ? (uri.href = newUri) : null
+            : history.replaceState(null, '', newUri);
     };
 
-    function WaitLoad(container, debounce, run) {
-        const observer = new MutationObserver(Debounce(() => { run() }, debounce));
+    const turnPage = (button) => {
+        jumpMark = true;
+        button.click();
+    };
+    function triggerTurnPage(event, buttons) {
+        const key = event.key;
+        if (key === "ArrowLeft" && !jumpMark) {
+            turnPage(buttons[0]);
+        } else if (key === "ArrowRight" && !jumpMark) {
+            turnPage(buttons[1]);
+        }
+    };
+
+    function waitLoad(container, debounce, run) {
+        const observer = new MutationObserver(_debounce(() => { run() }, debounce));
         observer.observe(container, { subtree: true, childList: true, attributes: true, characterData: true });
     };
 
-    function WaitElem(selector, found, all = false) {
+    function waitElem(selector, found, all = false) {
         const observer = new MutationObserver(() => {
             const result = all ? document.querySelectorAll(selector) : document.querySelector(selector);
             if (all ? result.length > 0 : result) {
@@ -102,4 +122,65 @@
         observer.observe(document, { subtree: true, childList: true });
     };
 
+    function onUrlChange(callback, timeout = 15) {
+        let timer;
+        let cleaned = false;
+        let support_urlchange = false;
+
+        const originalPushState = history.pushState;
+        const originalReplaceState = history.replaceState;
+
+        const eventHandler = {
+            urlchange: () => trigger('urlchange'),
+            popstate: () => trigger('popstate'),
+            hashchange: () => trigger('hashchange')
+        };
+
+        function trigger(type) {
+            clearTimeout(timer);
+            if (!support_urlchange && type === 'urlchange') support_urlchange = true;
+            timer = setTimeout(() => {
+                if (support_urlchange) off(false, true);
+                callback({
+                    type: type,
+                    url: location.href,
+                    domain: location.hostname
+                });
+            }, Math.max(15, timeout));
+        };
+
+        function off(all = true, clean = false) {
+            if (clean && cleaned) return;
+
+            clearTimeout(timer);
+            history.pushState = originalPushState;
+            history.replaceState = originalReplaceState;
+            window.removeEventListener('popstate', eventHandler.popstate);
+            window.removeEventListener('hashchange', eventHandler.hashchange);
+            all && window.removeEventListener('urlchange', eventHandler.urlchange);
+
+            cleaned = true;
+        };
+
+        window.addEventListener('urlchange', eventHandler.urlchange);
+        window.addEventListener('popstate', eventHandler.popstate);
+        window.addEventListener('hashchange', eventHandler.hashchange);
+        history.pushState = function () {
+            originalPushState.apply(this, arguments);
+            trigger('pushState');
+        };
+        history.replaceState = function () {
+            originalReplaceState.apply(this, arguments);
+            trigger('replacestate');
+        };
+
+        return { off };
+    };
+
+    // 初次執行
+    main(location.href);
+    // 監聽網址變化
+    onUrlChange(change => {
+        main(change.url);
+    });
 })();
