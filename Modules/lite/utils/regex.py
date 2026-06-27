f1860dc71754c03eff85ff0794b346fddbbf1063
Canaan-HS <s3459897@gmail.com>
2026-06-28 05:57:18 +0800

    變數名修改

--- Modules/lite/utils/regex.py
+++ Modules/lite/utils/regex.py
@@ -1,5 +1,5 @@
 from ..bootstrap import re
 
-illegal_regex = re.compile(r'[<>:"/\\|?*\x00-\x1F]')
-parse_regex = re.compile(r"(\d{8,10})(?:&searchtext=(.*))?")
-link_regex = re.compile(r"^(?:https://steamcommunity\.com/sharedfiles/filedetails/\?id=\d{8,10}.*|\d{8,10})$")
+ILLEGAL_REGEX = re.compile(r'[<>:"/\\|?*\x00-\x1F]')
+PARSE_REGEX = re.compile(r"(\d{8,10})(?:&searchtext=(.*))?")
+LINK_REGEX = re.compile(r"^(?:https://steamcommunity\.com/sharedfiles/filedetails/\?id=\d{8,10}.*|\d{8,10})$")
