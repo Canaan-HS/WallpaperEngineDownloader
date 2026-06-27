75660f72c30c47adb3861d1f08c3a327abee69b4
Canaan-HS <s3459897@gmail.com>
2026-06-28 05:54:13 +0800

    添加 QRCode 登入功能依賴

--- Modules/lite/utils/account.py
+++ Modules/lite/utils/account.py
@@ -1,5 +1,6 @@
 from ..bootstrap import base64
 
+# ! 為了簡化語法, 將格式改為 {key: {key: value}}
 account_dict = {
     key: {key: base64.b64decode(value).decode("utf-8")}
     for key, value in {
@@ -12,4 +13,15 @@ account_dict = {
     }.items()
 }
 
+# 判斷觸發 QR 登錄, 與登入後標記
+QR_KEY = "QRCodeLogin"
+LOGIN_KEY = "-remember-password"
+
+account_dict = {QR_KEY: {QR_KEY: ""}, **account_dict}
 account_list = list(account_dict.keys())
+
+
+def add_login_account(account):
+    account_dict.update({account: {account: LOGIN_KEY}})
+    account_list.clear()
+    account_list.extend(account_dict.keys())
